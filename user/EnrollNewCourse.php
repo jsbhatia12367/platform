49c036f052cf7678945cfb37e52ec65ea97c0013
unknown <kavish.agnihotri@iiitb.org>
2021-02-26 16:36:19 +0530

    Recent Changes

--- user/EnrollNewCourse.php
+++ user/EnrollNewCourse.php
@@ -1,86 +1,116 @@
-<?php include 'userinfo_user.php';?>
+<?php include 'userinfo_user.php'; ?>
 <!DOCTYPE html>
 <html lang="en-CA" class="no-js">
+
 <head>
-<script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
-<link href='../css/studentStyle.css' rel='stylesheet' type="text/css"/>
-<link href='../css/admin_table.css' rel='stylesheet' type="text/css"/>
-<script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
-<svg style="display:none;">
-</svg>
-</head>
-<body>
-  <header class="page-header">
-    <?php include 'LeftMenu.php';?>
-    
-  </header>
-  <section class="page-content">
-    <section class ="grid">
-      <article style="height: 550px">
-        <div class="main__container">
-          <div class="main__title">
-              <div class="main__greeting">
-                <h1>Student Information</h1>
-
-  <form method="post" action="../logic.php">
-  
-     
-    <div class="form-group">
-      <label for="selected_course">Available Courses:</label>
-      <select name="selected_course" id="selected_course">
-        <!-- Query 1 - Fetch Available courses(course_id) for perticular user from courses - enroll -->
-        <!-- Query 2 - Fetch Course Name for above course name -->
-
-       <!--  <option value="1">Database</option>
-        <option value="4">Compiler</option>
-        <option value="6">Algo</option> -->
-         <? php
-         // $dbconn = pg_connect("host=localhost port=5432 dbname=platform user=postgres password=postgres");  
-         // $dd_res = pg_query($dbconn,"SELECT * FROM public.courses"); 
-         // while($r=pg_fetch_row($dd_res))
-         // { 
-         //       echo "<option value='$r'> $r </option>";
-         // }
-
-  <?php
-        $db = pg_connect("host=localhost port=5432 dbname=platform user=postgres password=postgres"); 
-        $sql = pg_query(sprintf("SELECT * FROM public.courses where course_id NOT IN (SELECT course_id FROM public.enroll WHERE emailaddress='".pg_escape_string($Email)."')")) ;       
-        while ($row = pg_fetch_assoc($sql)) {
-            echo '<option value="'.htmlspecialchars($row['course_id']).'">'.htmlspecialchars($row['course_name']).'</option>';
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.5/js/bootstrap.min.js"></script>
+  <link href='../css/studentStyle.css' rel='stylesheet' type="text/css" />
+  <!-- <link href='../css/admin_table.css' rel='stylesheet' type="text/css" /> -->
+  <link rel="stylesheet" id="bootstrap-css" href="https://cdnjs.cloudflare.com/ajax/libs/twitter-bootstrap/4.1.3/css/bootstrap.min.css?ver=67c90ffd8417a442ac33ffaa4a4ee97a" type="text/css" media="all">
+  <link rel="stylesheet" id="site_styles-css" href="../css/main_styles.css?ver=1.7" type="text/css" media="all">
+  <script src="https://code.iconify.design/1/1.0.7/iconify.min.js"></script>
+  <script src="http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.min.js"></script>
+  <svg style="display:none;"></svg>
+  <script type="text/javascript">
+    function myFunction(clicked_id) {
+      $.ajax({
+        type: "POST",
+        url: 'ajax.php',
+        data: {
+          action: 'enroll_this',
+          course_id: clicked_id,
+          Email: 'kavish.agnihotri@gmail.com'
+        },
+        success: function(html) {
+          location.reload();
         }
-        pg_close($db);
-  ?>
-          
 
-        ?>
-      </select>
+      });
+    }
+  </script>
+</head>
+
+<body>
+  <div class="row">
+    <div class='col-3'>
+      <header class="page-header">
+        <?php include 'LeftMenu.php'; ?>
+
+      </header>
     </div>
+    <div class='col-9'>
+      <div id="page" class="site">
+        <div id="content" class="site-content">
+          <div id="skip-anchor" tabindex="-1"></div>
+          <section class="upcoming-courses">
+              
+            <div class="container">
+              <div class="row">
 
-    <input type="submit" name="submit_enroll_courses" class="btn btn-primary" value="Submit">
 
-  </form>
+
+                <?php
+
+                $db = pg_connect("host=localhost port=5432 dbname=platform user=postgres password=postgres");
+                $sql = pg_query(sprintf("SELECT * FROM public.courses where course_id NOT IN (select course_id From public.enroll where emailaddress='".$_SESSION['Email']."');"));
+
+
+                while ($row = pg_fetch_assoc($sql)) {
+                  echo "
+                          <div class='col-12 col-md-6 card-container'>
+                            <div id='tribe-event-content--5068' class='card tribe-events-single events-single-card' data-filter-container=''>
+                              <div class='location-meta' data-location='online'></div>
+                              <div class='tags' data-filter-target='' data-tags='online'></div>
+                              <div class='card__header'>
+                                <div class='card__title title-4 tribe-events-single-event-title'>" . htmlspecialchars($row['course_name']) . "</div> 
+                                </div>
+
+                              <div class='card__body small'>
+                              
+
+                                <table class='details'>
+                                  <tbody>
+                                    <tr>
+                                      <th>Start</th>
+                                      <td>" . htmlspecialchars($row['start_date']) . "</td>
+                                    </tr>
+                                    <tr>
+                                      <th>End</th>
+                                      <td>" . htmlspecialchars($row['end_date']) . "</td>
+                                    </tr>
+                                  </tbody>
+                                </table>
+                              </div>
+
+                              <div class='card__footer'>
+                                <button class='btn btn-primary' id='" . $row['course_id'] . "' onclick='myFunction(this.id)'>Enroll</button>
+                              </div>
+                            </div><!-- #tribe-events-content -->
+                          </div>";
+                }
+
+                ?>
+
+
+
               </div>
-              </div>
-            <table class="content-table">
-              <thead>
-                <tr>
-                <th>Sr.No</th>
-                <th>Course Name</th>
-                </tr>
-              </thead>
-              <tbody>
-         
-                  <tr>
-                    <td></td>
-                    <td><a href="../Courses.php"></a></td>
-                 
-             
-              </tbody>
-            </table>
-          </div>
-        </article>
-    </section>
-  </section>
+            </div>
+
+          </section>
+
+
+
+
+
+        </div><!-- #content -->
+
+
+
+      </div><!-- #page -->
+    </div>>
+  </div>
+
+
 </body>
 <footer class="page-footer">
 </footer>
--- user/ajax.php
+++ user/ajax.php
@@ -1,5 +1,12 @@
 <?php
+session_start();
 $db = pg_connect("host=localhost port=5432 dbname=platform user=postgres password=postgres");
 if($_POST['action'] == 'call_this') {
   pg_query(sprintf("UPDATE public.enroll SET completed = true WHERE course_id =". $_POST['course_id']." AND emailaddress='".$_POST['emailaddress']."' ;"));
 }
+
+if($_POST['action'] == 'enroll_this'){
+  pg_query(sprintf("insert into public.enroll(course_id,emailaddress) values(".$_POST['course_id'].",'".$_SESSION['Email']."');"));
+}
+
+?>
